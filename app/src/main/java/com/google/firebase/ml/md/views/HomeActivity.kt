ef7f7edc762c4dcef1849e523ecc16b41b2b2c7e
조연우 <wooisso@naver.com>
2020-06-21 20:07:18 +0900

    Sound Picture Home Activity 변경

--- app/src/main/java/com/google/firebase/ml/md/views/HomeActivity.kt
+++ app/src/main/java/com/google/firebase/ml/md/views/HomeActivity.kt
@@ -2,7 +2,6 @@ package com.google.firebase.ml.md.views
 
 import android.content.Context
 import android.content.Intent
-import android.graphics.drawable.Drawable
 import android.nfc.NdefMessage
 import android.nfc.NfcAdapter
 import android.os.Bundle
@@ -12,10 +11,6 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ml.md.R
 import com.google.firebase.ml.md.models.Data
@@ -54,22 +49,10 @@ class HomeActivity : AppCompatActivity() {
         rv_touch_grid.adapter = gridAdapter
         Glide.with(this)
                 .load(datum.imgUrl)
-                .listener(object : RequestListener<Drawable> {
-                    override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
-                        pb_loading.visibility = View.GONE
-                        iv_img.alpha = 1f
-                        return false
-                    }
-
-                    override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
-                        pb_loading.visibility = View.GONE
-                        iv_img.alpha = 1f
-                        return false
-                    }
-
-                })
                 .centerCrop()
                 .into(iv_img)
+        pb_loading.visibility = View.GONE
+        iv_img.alpha = 1f
     }
 
     override fun onNewIntent(intent: Intent) {
